3b8333742fe1dd84e40cb9c664d87d0b94ac3707
ahmad <ahmad.imran.2904@gmail.com>
2026-07-16 20:10:23 +0500

    Header Done

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -53,7 +53,7 @@ const Header = () => {
                 className={`block relative font-code text-xl uppercase font-semibold text-n-1 transition-colors hover:text-color-1 ${
                   item.onlyMobile ? 'lg:hidden' : ''
                 } px-6 py-6 md:py-8 lg:-mr-0.25 lg:text-xs lg:font-semibold ${
-                  item.url === pathname.hash ? 'z-2 lg:text-n-1' : 'lg:text-n-1/50'
+                  item.url === pathname.hash ? 'z-2 lg:text-n-1' : 'lg:text-n-1/55'
                 } lg:leading-5 lg:hover:text-n-1 xl:px-12`}
               >
                 {item.title}
